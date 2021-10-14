488c84c29535180a1abdb44c21cf25bb7cd08fc3
franbcn93 <franciscoparisalbero@gmail.com>
2021-10-14 14:09:07 +0200

    Arrays

---
+++ src/Arrays.java
@@ -0,0 +1,24 @@
+import javax.swing.JOptionPane;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// int [] miMatriz = new int[5];
+		// miMatriz[3] = 10;
+		
+		int [] miMatriz = {1,2,3,4,6,3,5,9};
+		
+		 
+		
+		for (int i = 0; i < miMatriz.length; i++) {
+			JOptionPane.showConfirmDialog(null, miMatriz[i]);
+		}
+		
+		
+		
+		
+	}
+
+}
